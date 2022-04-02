0db195a30929a08ee2f167017398d104cba4a585
flyingfox <1372679708@qq.com>
2022-04-02 15:41:23 +0800

    'genetr'

--- R/gene_tr.R
+++ R/gene_tr.R
@@ -14,6 +14,8 @@ gene_tr = function(type,ID){
     checkmate::assert_integer(ID)
     len = length(ID)
     SYMBOL = NA
+    no_exit = NA
+    N = 1
     for (x in 1:len) {
 
       hanghao = which(tr_data$ENTREZID == ID[x], arr.ind = T)
@@ -21,32 +23,38 @@ gene_tr = function(type,ID){
       if (length(hanghao) != 0) {
         SYMBOL[x] = tr_data[hanghao,5]
       }else{
-        options(warn = 1)
-        warning(paste('This gene cannot be found', ID[x], '\n'))
+        no_exit[N] = ID[x]
+        N = N + 1
       }
 
     }
 
     df = cbind(ID, SYMBOL)
-
+    options(warn = 1)
+    warning(paste(length(no_exit/len) ,'% of input gene IDS are fail tp map...'))
     return(df)
+
   }else if(type == 'SYMBOL') {
     ## 如果输入是SYMBOL
     checkmate::assert_character(ID)
     len = length(ID)
     ENTREZID = NA
+    no_exit = NA
+    N = 1
     for (x in 1:len) {
       hanghao = which(tr_data$SYMBOL == ID[x], arr.ind = T)
 
       if (length(hanghao) != 0) {
         ENTREZID[x] = tr_data[hanghao,2]
       }else{
-        options(warn = 1)
-        warning(paste('This gene cannot be found', ID[x], '\n'))
+        no_exit[N] = ID[x]
+        N = N + 1
       }
     }
 
     df = cbind(ID, ENTREZID)
+    options(warn = 1)
+    warning(paste(length(no_exit/len) ,'% of input gene IDS are fail tp map...'))
     return(df)
   }else {
     paste('Please enter ENTREZID or SYMBOL')
