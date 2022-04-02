65a11bc0f0c475cc0006998bc55dcb4e11dca48f
flyingfox <1372679708@qq.com>
2022-04-02 15:29:54 +0800

    'genetr'

---
+++ R/gene_tr.R
@@ -0,0 +1,59 @@
+#' Gene ID transform
+#'
+#' @param type ENTREZID or SYMBOLID
+#' @param ID Set of genes named ENTREZID or SYMBOLID
+#' @return ENTREZID or SYMBOLID
+#' @keywords Gene ID transform
+#' @export
+
+
+gene_tr = function(type,ID){
+
+  if (type == 'ENTREZID') {
+    ## 如果输入是ENTREZID
+    checkmate::assert_integer(ID)
+    len = length(ID)
+    SYMBOL = NA
+    for (x in 1:len) {
+
+      hanghao = which(tr_data$ENTREZID == ID[x], arr.ind = T)
+
+      if (length(hanghao) != 0) {
+        SYMBOL[x] = tr_data[hanghao,5]
+      }else{
+        options(warn = 1)
+        warning(paste('This gene cannot be found', ID[x], '\n'))
+      }
+
+    }
+
+    df = cbind(ID, SYMBOL)
+
+    return(df)
+  }else if(type == 'SYMBOL') {
+    ## 如果输入是SYMBOL
+    checkmate::assert_character(ID)
+    len = length(ID)
+    ENTREZID = NA
+    for (x in 1:len) {
+      hanghao = which(tr_data$SYMBOL == ID[x], arr.ind = T)
+
+      if (length(hanghao) != 0) {
+        ENTREZID[x] = tr_data[hanghao,2]
+      }else{
+        options(warn = 1)
+        warning(paste('This gene cannot be found', ID[x], '\n'))
+      }
+    }
+
+    df = cbind(ID, ENTREZID)
+    return(df)
+  }else {
+    paste('Please enter ENTREZID or SYMBOL')
+  }
+
+
+}
+
+
+
